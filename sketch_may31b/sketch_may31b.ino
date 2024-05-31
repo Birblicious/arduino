f9de334f4195d0969d4797f00948ee51b7aa9849
Arda Burak Ekmekçi <ardaburak1992@hotmail.com>
2024-05-31 20:49:40 +0200

    first sketch, loops

--- sketch_may31b/sketch_may31b.ino
+++ sketch_may31b/sketch_may31b.ino
@@ -1,9 +1,14 @@
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Hello");
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  String learningTime = "Learning time: ";
+  delay(1000);
+  Serial.println(learningTime += millis());
 
 }
