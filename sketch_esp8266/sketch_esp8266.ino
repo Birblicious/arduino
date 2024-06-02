a210a7b3689d18af11b47a6a855d254e99a020a6
Arda Burak Ekmekçi <ardaburak1992@hotmail.com>
2024-06-02 19:43:37 +0200

    http call is made, https awaits

--- sketch_esp8266/sketch_esp8266.ino
+++ sketch_esp8266/sketch_esp8266.ino
@@ -1,4 +1,7 @@
 #include <ESP8266WiFi.h>
+#include <Arduino_JSON.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
 
 void setup()
 {
@@ -17,6 +20,43 @@ void setup()
 
   Serial.print("Connected, IP address: ");
   Serial.println(WiFi.localIP());
+
+// 
+Serial.println("BEFORE GET");
+String sensorReadings;
+sensorReadings = httpGETRequest("https://date.nager.at/api/v3/publicholidays/2024/AT");
+Serial.println(sensorReadings);
+Serial.println("AFTER GET");
+}
+
+String httpGETRequest(const char* serverName) {
+  WiFiClient client;
+  HTTPClient http;
+    
+  // Your IP address with path or Domain name with URL path 
+  http.begin(client, serverName);
+  
+  // If you need Node-RED/server authentication, insert user and password below
+  //http.setAuthorization("REPLACE_WITH_SERVER_USERNAME", "REPLACE_WITH_SERVER_PASSWORD");
+  
+  // Send HTTP POST request
+  int httpResponseCode = http.GET();
+  
+  String payload = "{}"; 
+  
+  if (httpResponseCode>0) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    payload = http.getString();
+  }
+  else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+  // Free resources
+  http.end();
+
+  return payload;
 }
 
 void loop() {}
